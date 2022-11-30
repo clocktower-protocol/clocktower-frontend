0fd0c9a263a5e8e67cf00848d42518e25c07e880
Hugo Marx <hugo@clocktower.finance>
2022-11-29 22:20:53 -0600

    added new hour select menu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react'
-import { Form, Navbar, Container, Nav, Button, Table } from 'react-bootstrap';
+import { Form, Navbar, Container, Nav, Button, Table, Row, Col } from 'react-bootstrap';
 import Web3 from 'web3'
 import './App.css';
 import {CLOCKTOWER_ABI, CLOCKTOWER_ADDRESS} from "./config"; 
+import {HourSelect} from "./hourSelect.js";
 
 class App extends Component {
-
-
-
-  componentWillMount() {
-    //const web3 = new Web3("http://localhost:8545")
-    this.loadBlockchainData()
-  }
 
   
   async loadBlockchainData() {
@@ -23,6 +17,8 @@ class App extends Component {
     const clocktower = new web3.eth.Contract(CLOCKTOWER_ABI, CLOCKTOWER_ADDRESS);
     this.setState({clocktower});
   }
+
+  //Metamask-----------------------------------------
 
   //changes wallet button based on status
   walletButtonClick() {
@@ -44,7 +40,6 @@ class App extends Component {
       console.log(
         "connectWallet called"
       )
-
       
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
@@ -58,14 +53,53 @@ class App extends Component {
       console.log(error);
     }
   }
-  
 
+  //Form------------------------------------------------
+  receiverChange(event) {
+    this.setState({formAddress: event.target.value});
+  }
+  dateChange(event) {
+    this.setState({formDate: event.target.value});
+  }
+  amountChange(event) {
+    this.setState({formAmount: event.target.value});
+  }
+  hourChange(event) {
+    this.setState({formSelect: event.target.value});
+    console.log(
+      event.target.value
+    )
+  }
+  submitForm(event) {
+    const form = event.currentTarget;
+
+    event.preventDefault();
+    event.stopPropagation();
+    
+    //validates data
+
+  };
+
+  
+  //initializes values
   constructor(props) {
     super(props)
+
+    //loads blockchain data
+    //this.loadBlockchainData()
     //hardhat test account
     this.state = { account: '' }
     this.state = {buttonClicked: false}
     this.connectWallet = this.connectWallet.bind(this);
+
+    //form default info
+    this.state = {formAddress: "0x0", formDate: "947462400", formAmount: 0.00};
+    //form methods
+    this.receiverChange = this.receiverChange.bind(this);
+    this.dateChange = this.dateChange.bind(this);
+    this.amountChange = this.amountChange.bind(this);
+    this.hourChange = this.hourChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
   }
 
   render() {
@@ -82,24 +116,89 @@ class App extends Component {
             </Nav>
           </Container>
         </Navbar>
+    
       <div className="clockBody">
+      <div className="clockFormDiv">
+        <Form className="mb-3" onSubmit={this.submitForm}>
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" controlId="formAddress" value={this.state.formAddress} onChange={this.receiverChange}>
+            <Form.Label>Address to Send to:</Form.Label>
+            <Form.Control type="input" placeholder="receiver" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3" controlId="formAmount" value={this.state.formAmount} onChange={this.amountChange}>
+              <Form.Label>Amount:</Form.Label>
+              <Form.Control type="input" placeholder="amount" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" controlId="formDate" value={this.state.formDate} onChange={this.dateChange}>  
+              <Form.Label>Date: (MM/DD/YYYY)</Form.Label>
+              <Form.Control type="input" placeholder="date" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3" controlId="formSelect" value={this.state.formSelect} onChange={this.hourChange}>
+            <Form.Label>Hour:</Form.Label>
+            <Form.Select>
+                <option>Select which hour</option>
+                <option value="1">1:00 AM</option>
+                <option value="2">2:00 AM</option>
+                <option value="3">3:00 AM</option>
+                <option value="4">4:00 AM</option>
+                <option value="5">5:00 AM</option>
+                <option value="6">6:00 AM</option>
+                <option value="7">7:00 AM</option>
+                <option value="8">8:00 AM</option>
+                <option value="9">9:00 AM</option>
+                <option value="10">10:00 AM</option>
+                <option value="11">11:00 AM</option>
+                <option value="12">12:00 AM</option>
+                <option value="13">1:00 PM</option>
+                <option value="14">2:00 PM</option>
+                <option value="15">3:00 PM</option>
+                <option value="16">4:00 PM</option>
+                <option value="17">5:00 PM</option>
+                <option value="18">6:00 PM</option>
+                <option value="19">7:00 PM</option>
+                <option value="20">8:00 PM</option>
+                <option value="21">9:00 PM</option>
+                <option value="22">10:00 PM</option>
+                <option value="23">11:00 PM</option>
+                <option value="1424">12:00 PM</option>
+              </Form.Select>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col align="center"><Button type="submit">Submit</Button></Col>
+        </Row>
+        </Form>
+        </div>
+        {/*
         <div className="clockFormDiv">
-          <Form className="mb-3">
-          <Form.Group className="mb-3" controlId="formAddress">
+          <Form className="mb-3" onSubmit={this.submitForm}>
+          <Form.Group className="mb-3" controlId="formAddress" value={this.state.formAddress} onChange={this.receiverChange}>
             <Form.Label>Address to Send to:</Form.Label>
             <Form.Control type="input" placeholder="receiver" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formDate">  
+          <Form.Group className="mb-3" controlId="formDate" value={this.state.formDate} onChange={this.dateChange}>  
             <Form.Label>Date:</Form.Label>
             <Form.Control type="input" placeholder="date" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formAmount">
+          <Form.Group className="mb-3" controlId="formAmount" value={this.state.formAmount} onChange={this.amountChange}>
             <Form.Label>Amount:</Form.Label>
             <Form.Control type="input" placeholder="amount" />
           </Form.Group>
           <Button type="submit">Submit form</Button>
           </Form>
         </div>
+            */}
+        
         <div className="clockTableDiv">
           <Table striped bordered hover size="sm" className="clockTable">
             <thead>

4ff866848ce87566d52b734c5a7da43c98ad02c6
Hugo Marx <hugo@clocktower.finance>
2023-02-01 15:23:28 -0600

    approval part of subscribe function almost working

--- src/routes/publicsubscription.jsx
+++ src/routes/publicsubscription.jsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState, useCallback} from 'react'
 import {Alert, Card, ListGroup, Button} from 'react-bootstrap';
-import { useOutletContext, useParams} from "react-router-dom";
+import { useOutletContext, useParams, useNavigate} from "react-router-dom";
 import Web3 from 'web3'
-import {CLOCKTOWERSUB_ABI, CLOCKTOWERSUB_ADDRESS, FREQUENCY_LOOKUP, CLOCKTOKEN_ADDRESS, CLOCKTOKEN_ABI, INFINITE_APPROVAL, TOKEN_LOOKUP} from "../config"; 
+import {CLOCKTOWERSUB_ABI, CLOCKTOWERSUB_ADDRESS, FREQUENCY_LOOKUP, CLOCKTOKEN_ADDRESS, CLOCKTOKEN_ABI, INFINITE_APPROVAL, TOKEN_LOOKUP, ZERO_ADDRESS} from "../config"; 
 
 const PublicSubscription = () => {
 
@@ -17,6 +17,9 @@ const PublicSubscription = () => {
     const [amount, setAmount] = useState(0)
     const [frequencyName, setFrequencyName] = useState("Monthly")
     const [tickerName, setTickerName] = useState("CLOCK")
+    const [token, setToken] = useState(ZERO_ADDRESS)
+    const [tokenABI, setTokenABI] = useState(CLOCKTOKEN_ABI)
+    const [alertType, setAlertType] = useState("danger")
 
     
     //loads provider subscription list upon receiving parameter
@@ -27,6 +30,7 @@ const PublicSubscription = () => {
             if(token.address == tokenAddress) {
                 return token.ticker
             }
+           // return true
             })
          }
   
@@ -36,6 +40,19 @@ const PublicSubscription = () => {
             if(frequencyIndex == frequencyObject.index) {
                 return frequencyObject.name
             }
+           // return true
+            })
+        }
+
+        //looks up token abi
+        const abiLookup = (tokenAddress) => {
+            //sets abi
+            return TOKEN_LOOKUP.map((token) => {
+                if(token.address === tokenAddress){
+                    //console.log(token.ABI)
+                    return token.ABI
+                }
+            return true
             })
         }
 
@@ -45,6 +62,8 @@ const PublicSubscription = () => {
             setAmount(Web3.utils.fromWei(result.amount))
             setFrequencyName(frequencyLookup(result.frequency))
             setTickerName(tickerLookup(result.token))
+            setTokenABI(abiLookup(result.token)[1])
+            setToken(result.token)
         })
 
         if(account != "-1"){
@@ -59,6 +78,110 @@ const PublicSubscription = () => {
     //gets contract interface
     const clocktowersub = new web3.eth.Contract(CLOCKTOWERSUB_ABI, CLOCKTOWERSUB_ADDRESS);
     const clocktoken = new web3.eth.Contract(CLOCKTOKEN_ABI, CLOCKTOKEN_ADDRESS);
+
+     //Creates alert
+     const alertMaker = () => {
+        if(alert) {
+        return (
+        <div className="alertDiv">
+        <Alert variant={alertType} align="center" onClose={() => setAlert(false)} dismissible>{alertText}</Alert>
+        </div>
+        )
+        }
+    }
+
+    //gets unlimited approval from user
+    const setInfiniteAllowance = useCallback(async () => {
+        let transactionParameters = {};
+        console.log(tokenABI)
+        //console.log(token)
+        //const web3 = new Web3(node)
+        const contract = new web3.eth.Contract(tokenABI, token);
+
+        const confirmTransaction = async (txHash) => {
+
+            //gets transaction details
+            const trx = await web3.eth.getTransaction(txHash)
+    
+            let isDone = false;
+            
+            //trys every five seconds to see if transaction is confirmed
+            isDone = setTimeout(async () => {
+    
+            if(trx.blockNumber) {
+                //turns off alert
+                setAlert(false)
+                setAlertType("danger")
+                //getAccountTransactions()
+                return true
+            }
+    
+            //return await this.confirmTransaction(txHash)
+            await confirmTransaction(txHash)
+            return false
+            },5*1000)
+    
+            
+            if(isDone) {
+            return true
+            } 
+        }
+
+        if(token !== ZERO_ADDRESS) {
+        
+            transactionParameters = {
+            to: token, // Required except during contract publications.
+            from: account, // must match user's active address.
+            data: contract.methods.approve(CLOCKTOWERSUB_ADDRESS, INFINITE_APPROVAL).encodeABI()
+        };
+
+            //get metamask to sign transaction 
+            try {
+            await window.ethereum.request({
+                method: "eth_sendTransaction",
+                params: [transactionParameters],
+            })
+            .then (async (txhash) => {
+                console.log(txhash)
+                
+                //turns on alert ahead of confirmation check loop so user doesn't see screen refresh
+                setAlertType("warning")
+                setAlert(true)
+                setAlertText("Transaction Pending...")
+                 
+                await confirmTransaction(txhash)
+            })
+
+            return {
+                status: "transaction cancelled!"
+            };
+            
+            } catch (error) {
+            return {
+                status: error.message
+            }
+            } 
+        
+        }
+    }, [account, setAlert, setAlertText, token, tokenABI, ZERO_ADDRESS, CLOCKTOWERSUB_ADDRESS, INFINITE_APPROVAL, web3, setAlertType]
+    )
+
+    //handles subscription button click and navigation
+    const subscribe = useCallback(async () => {
+
+        //first requires user to approve unlimited allowance
+        await setInfiniteAllowance()
+
+        //subscribes to subscription
+        /*
+        const transactionParameters = {
+            to: CLOCKTOWERSUB_ADDRESS, // Required except during contract publications.
+            from: account, // must match user's active address.
+            data: clocktowersub.methods.createSubscription(amount,token,description,frequency, dueDay).encodeABI(),
+        }
+        */
+        
+    })
 
     //setId(id)
 
@@ -111,25 +234,29 @@ const PublicSubscription = () => {
         )
     } else {
         return (
-            <div className="publicSub">
-            <Card style={{ width: '35rem' }}>
-                <Card.Body>
-                    <Card.Title align="center">Subscription</Card.Title>
-                    <Card.Text align="center">
-                    {subscription.description}
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroup.Item>Producer:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {subscription.provider}</ListGroup.Item>
-                    <ListGroup.Item>Amount: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{amount} {tickerName}</ListGroup.Item>
-                    <ListGroup.Item>Frequency: &nbsp;&nbsp;{frequencyName}</ListGroup.Item>
-                    <ListGroup.Item>Day Due: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{subscription.dueDay}</ListGroup.Item>
-                </ListGroup>
-                <Card.Body align="center">
-                    <Button>Subscribe</Button>
-                </Card.Body>
-            </Card>
+            <div> 
+            {alertMaker()}
+                <div className="publicSub">
+                <Card style={{ width: '35rem' }}>
+                    <Card.Body>
+                        <Card.Title align="center">Subscription</Card.Title>
+                        <Card.Text align="center">
+                        {subscription.description}
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroup.Item>Producer:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {subscription.provider}</ListGroup.Item>
+                        <ListGroup.Item>Amount: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{amount} {tickerName}</ListGroup.Item>
+                        <ListGroup.Item>Frequency: &nbsp;&nbsp;{frequencyName}</ListGroup.Item>
+                        <ListGroup.Item>Day Due: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{subscription.dueDay}</ListGroup.Item>
+                    </ListGroup>
+                    <Card.Body align="center">
+                        <Button onClick={subscribe}>Subscribe</Button>
+                    </Card.Body>
+                </Card>
+                </div>
             </div>
+
 
         )
     }

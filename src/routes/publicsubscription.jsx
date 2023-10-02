0e9765a57dd9a03bbdaef37ab3b1f43ac231df52
Hugo Marx <hugo@clocktower.finance>
2023-10-02 14:55:42 -0500

    removed verify button if domain is not set

--- src/routes/publicsubscription.jsx
+++ src/routes/publicsubscription.jsx
@@ -427,7 +427,7 @@ const PublicSubscription = () => {
                         <Button onClick={subscribe}>Subscribe</Button>
                     </Card.Body>
                     : ""}
-                    {(isProvider && !isDomainVerified) ?
+                    {(isProvider && !isDomainVerified && details.domain != "") ?
                     <>
                     <Card.Body align="center">
                         {/*

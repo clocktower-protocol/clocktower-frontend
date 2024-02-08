f2278c60d7329841f5ce214e0f35934bd2c3fac4
Hugo Marx <hugo@clocktower.finance>
2024-02-08 16:06:27 -0600

    weekly prov calendar events work

--- src/routes/calendar.jsx
+++ src/routes/calendar.jsx
@@ -16,7 +16,9 @@ const Calendar = () => {
     const { address } = useAccount()
 
     let emptyArray = []
+    
     const [provSubscriptionArray, setProvSubscriptionArray] = useState(emptyArray)
+    const [eventsArray, setEventsArray] = useState(emptyArray)
 
     //loads provider subscription list upon login
     useEffect(() => {
@@ -35,7 +37,7 @@ const Calendar = () => {
 
        //variable to pass scope so that the state can be set
        let accountSubscriptions = []
-       let timeArray = []
+       let tempEventsArray = []
 
        try{
        await readContract({
@@ -50,30 +52,59 @@ const Calendar = () => {
            //loops through each subscription
            for (var i = 0; i < accountSubscriptions.length; i++) {
 
-                //gets time information from each subscription
-                let dueDay = accountSubscriptions[i].subscription.dueDay
-                let frequency = accountSubscriptions[i].subscription.frequency
-                let now = dayjs()
-                console.log(now.day())
-                console.log(dueDay)
 
-                //creates calendar events based on frequency and dueDay
-                switch (frequency) {
-                    //weekly
-                    case 0:
-                        break
-                    //monthly
-                    case 1:
-                        break
-                    //quarterly
-                    case 2: 
-                        break
-                    //yearly
-                    case 3:
-                        break
-                    
+                if(accountSubscriptions[i].status === 0) {
+                    //gets time information from each subscription
+                    let dueDay = accountSubscriptions[i].subscription.dueDay
+                    let frequency = accountSubscriptions[i].subscription.frequency
+                    let now = dayjs()
+                    //console.log(now.day())
+                    //console.log(dueDay)
+
+                    //creates calendar events based on frequency and dueDay
+                    switch (frequency) {
+                        //weekly
+                        case 0:
+                            //adjusts for Sunday being 7
+                            if(dueDay === 7) {
+                                dueDay = 0
+                            }
+
+                            let difference
+
+                            //first due day is next week
+                            if(dueDay < now.day()){
+                                difference = (7 - now.day()) + dueDay
+                            } else {
+                                //first due date is this week
+                                difference = dueDay - now.day()
+                            }
+                            //adds the days to the next event
+                            let nextEvent = now.add(difference, 'day')
+                            //saves info to array
+                            tempEventsArray.push({title: "test", date: nextEvent.format('YYYY-MM-DD'), backgroundColor: "green"})
+                            //increments event by a week for the next two years and saves to array
+                            for (var j = 0; j < 105; j++) {
+                                nextEvent = nextEvent.add(7, 'd')
+                                //eventsArray.push({title: accountSubscriptions[i].subscription.id, date: nextEvent.format('YYYY-MM-DD')})
+                                tempEventsArray.push({title: accountSubscriptions[i].subscription.id, date: nextEvent.format('YYYY-MM-DD'), backgroundColor: "green"})
+                            }
+                            break
+                        //monthly
+                        case 1:
+                            break
+                        //quarterly
+                        case 2: 
+                            break
+                        //yearly
+                        case 3:
+                            break
+                        
+                    }
                 }
            }
+
+           setEventsArray(tempEventsArray)
      
        })
     
@@ -86,6 +117,7 @@ const Calendar = () => {
         <FullCalendar
         plugins={[ dayGridPlugin ]}
         initialView="dayGridMonth"
+        events={eventsArray}
       />
     )
 }

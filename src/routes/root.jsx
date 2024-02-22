8fdacd87e6dac2b6349c48badee3b81364ea6dc6
Hugo Marx <hugo@clocktower.finance>
2024-02-22 17:55:54 -0600

    token now checks time and refreshes

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -99,13 +99,37 @@ const Root = () => {
           console.log(error);
         })
       } else {
-        //checks if its expired
+        //checks if token has expired
         const savedToken = localStorage.getItem("clockAccess")
         const decodedToken = jwtDecode(savedToken)
+        
         console.log("current utc time  " + dayjs().utc().unix())
         console.log("token expiry  " + decodedToken.exp)
         console.log("difference  " + (decodedToken.exp - dayjs().utc().unix()))
         console.log(decodedToken)
+        console.log(typeof dayjs().utc().unix())
+        //gets token if out of date
+        if(decodedToken.exp < dayjs().utc().unix()) {
+          //gets token
+          let data = {
+            "id": 4
+          }
+
+          axios.post('http://138.197.26.60:3000/api/requesttoken', data, {headers: {
+            'Content-Type': 'application/json'
+            }
+          })
+          .then(function (response) {
+            console.log(response.data.token);
+            //stores token in local storage
+            localStorage.setItem("clockAccess", response.data.token)
+            console.log("token set")
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+        }
+        
         console.log("got existing token")
       }
     }

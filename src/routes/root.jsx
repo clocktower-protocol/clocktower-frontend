db08830577b406c06241c1f1ea0e2f111037b45d
Hugo Marx <hugo@clocktower.finance>
2024-02-23 13:13:02 -0600

    added try catch and async to fetchToken function

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -76,45 +76,16 @@ const Root = () => {
     }, [])
     */
 
-    const fetchToken = () => {
-       //if empty
-       if(localStorage.getItem("clockAccess") === null) {
-        console.log("not set")
-    
-        let data = {
-          "id": 4
-         }
-        //gets token
-        axios.post('http://138.197.26.60:3000/api/requesttoken', data, {headers: {
-          'Content-Type': 'application/json'
-          }
-        })
-        .then(function (response) {
-          console.log(response.data.token);
-          //stores token in local storage
-          localStorage.setItem("clockAccess", response.data.token)
-          console.log("token set")
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
-      } else {
-        //checks if token has expired
-        const savedToken = localStorage.getItem("clockAccess")
-        const decodedToken = jwtDecode(savedToken)
-        
-        console.log("current utc time  " + dayjs().utc().unix())
-        console.log("token expiry  " + decodedToken.exp)
-        console.log("difference  " + (decodedToken.exp - dayjs().utc().unix()))
-        console.log(decodedToken)
-        console.log(typeof dayjs().utc().unix())
-        //gets token if out of date
-        if(decodedToken.exp < dayjs().utc().unix()) {
-          //gets token
+    const fetchToken = async () => {
+      try{
+        //if empty
+        if(localStorage.getItem("clockAccess") === null) {
+          console.log("not set")
+      
           let data = {
             "id": 4
           }
-
+          //gets token
           axios.post('http://138.197.26.60:3000/api/requesttoken', data, {headers: {
             'Content-Type': 'application/json'
             }
@@ -128,9 +99,42 @@ const Root = () => {
           .catch(function (error) {
             console.log(error);
           })
+        } else {
+          //checks if token has expired
+          const savedToken = localStorage.getItem("clockAccess")
+          const decodedToken = jwtDecode(savedToken)
+          
+          console.log("current utc time  " + dayjs().utc().unix())
+          console.log("token expiry  " + decodedToken.exp)
+          console.log("difference  " + (decodedToken.exp - dayjs().utc().unix()))
+          console.log(decodedToken)
+          console.log(typeof dayjs().utc().unix())
+          //gets token if out of date
+          if(decodedToken.exp < dayjs().utc().unix()) {
+            //gets token
+            let data = {
+              "id": 4
+            }
+
+            axios.post('http://138.197.26.60:3000/api/requesttoken', data, {headers: {
+              'Content-Type': 'application/json'
+              }
+            })
+            .then(function (response) {
+              console.log(response.data.token);
+              //stores token in local storage
+              localStorage.setItem("clockAccess", response.data.token)
+              console.log("token set")
+            })
+            .catch(function (error) {
+              console.log(error);
+            })
+          }
+          
+          console.log("got existing token")
         }
-        
-        console.log("got existing token")
+      } catch (error){
+        console.error(error)
       }
     }
     

01f9557a362fdc34272cb4bfba3c395c6282db7f
Hugo Marx <hugo@clocktower.finance>
2025-05-20 12:20:37 -0500

    changed graph to proxy server

--- src/apolloclient.js
+++ src/apolloclient.js
@@ -1,8 +1,16 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const SUBGRAPH_URL = import.meta.env.VITE_SUBGRAPH_URL
+//const API_KEY = import.meta.env.VITE_SUBGRAPH_API_KEY
 
 export const apolloClient = new ApolloClient({
-    link: new HttpLink({ uri: SUBGRAPH_URL }),
+    link: new HttpLink({ 
+        uri: SUBGRAPH_URL,
+        /*
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`
+        }
+        */
+    }),
     cache: new InMemoryCache(),
 });

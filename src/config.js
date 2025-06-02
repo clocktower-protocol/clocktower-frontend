769469ca7cf4f8d567bf2714e92f8574a90aa17e
Hugo Marx <hugo@clocktower.finance>
2025-06-02 15:54:49 -0500

    redeploy to add vars

--- src/config.js
+++ src/config.js
@@ -50,7 +50,7 @@ export const TOKEN_LOOKUP = [
     decimals: 18,
     icon: SepoliaLogo
   }, 
-  */
+  */ 
   {
     //address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     address: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",

cb3ec2ee4947d5e10f5d7d551e3fd31c30647f11
Hugo Marx <hugo@clocktower.finance>
2025-06-15 18:23:45 -0500

    converted root

---
+++ src/routes/root.tsx
@@ -0,0 +1,210 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { Navbar, Container, Nav, Button, Row, Col, Modal, Stack, Alert, NavDropdown } from 'react-bootstrap';
+import { Outlet, useNavigate, useLocation, Link } from "react-router";
+import { config } from '../wagmiconfig';
+import { CHAIN_LOOKUP, WALLET_LOOKUP, ADMIN_ACCOUNT } from '../config';
+import Icon from '../components/Icon';
+import { v4 as uuidv4 } from 'uuid';
+import styles from '../css/clocktower.module.css';
+import ThemeToggle from '../components/ThemeToggle';
+import { useAccount, useConnect, useAccountEffect, useWatchPendingTransactions, useSwitchChain } from 'wagmi';
+import { createApolloClient } from '../apolloclient';
+import { ApolloProvider } from '@apollo/client';
+
+const Root: React.FC = () => {
+    const { address, isConnected, isDisconnected, chainId } = useAccount({ config });
+    const [client, setClient] = useState(() => createApolloClient(chainId));
+
+    useEffect(() => {
+        if (chainId) {
+            setClient(createApolloClient(chainId));
+        }
+    }, [chainId]);
+
+    const { chains, switchChain } = useSwitchChain();
+    const location = useLocation();
+    const [account, setAccount] = useState<string>("");
+    const [showWalletChoice, setShowWalletChoice] = useState<boolean>(false);
+    const [selectedChain, setSelectedChain] = useState<number>(0);
+
+    const navigate = useNavigate();
+    const handleOnClickCalendar = useCallback(() => navigate('/calendar', { replace: true }), [navigate]);
+    const handleOnClickAccount = useCallback(() => navigate('/account/' + account, { replace: true }), [navigate, account]);
+    const handleOnClickSubscriptions = useCallback(() => navigate(`/subscriptions/created`, { replace: true }), [navigate]);
+    const handleOnClickAdmin = useCallback(() => navigate('/admin', { replace: true }), [navigate]);
+    const accountSwitch = useCallback((passedAddress: string) => navigate('/account/' + passedAddress), [navigate]);
+    const linkToMain = useCallback(() => navigate('/', { replace: true }), [navigate]);
+
+    const handleClose = () => {
+        setShowWalletChoice(false);
+    };
+
+    const handleShow = () => setShowWalletChoice(true);
+
+    useAccountEffect({
+        config,
+        onConnect() {
+            console.log('Connected!');
+        },
+        onDisconnect() {
+            console.log('Disconnected!');
+            linkToMain();
+        },
+    });
+
+    useWatchPendingTransactions({
+        onError(error) {
+            console.log('Error', error);
+        }
+    });
+
+    const { connect, connectors, isPending } = useConnect({ config });
+
+    const supportedChainIds = config.chains.map(chain => chain.id);
+
+    useEffect(() => {
+        setAccount(address || "");
+        if (address !== undefined && address !== account) {
+            if ((location.pathname.slice(0, 20) !== "/public_subscription") && (location.pathname.slice(0, 8) !== "/history")) {
+                accountSwitch(address);
+            }
+        }
+    }, [address, chains, accountSwitch, location, account]);
+
+    const adminAccount = ADMIN_ACCOUNT;
+
+    const walletButtonClick = () => {
+        handleShow();
+    };
+
+    const changeChain = (chain_id: number) => {
+        switchChain({ chainId: chain_id });
+        const chainIndex = CHAIN_LOOKUP.findIndex((lchain) => lchain.id === chain_id);
+        if (chainIndex !== -1) {
+            setSelectedChain(chainIndex);
+        }
+    };
+
+    useEffect(() => {
+        if (chainId) {
+            CHAIN_LOOKUP.forEach((lchain, index) => {
+                if (lchain.id === chainId) {
+                    setSelectedChain(index);
+                }
+            });
+        }
+    }, [chainId]);
+
+    return (
+        <ApolloProvider client={client}>
+            <div key={"root"}>
+                <Modal show={showWalletChoice} onHide={handleClose} className={styles.wallet_modal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title className="w-100 text-center">Choose a Wallet</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Container>
+                            <Stack gap={3}>
+                                {connectors.map((connector) => (
+                                    <Row key={uuidv4()}>
+                                        <Col key={uuidv4()} md="auto">
+                                            {WALLET_LOOKUP.map((lWallet) => {
+                                                if (lWallet.id === connector.id) {
+                                                    return <Icon key={uuidv4()} icon={lWallet.icon}></Icon>;
+                                                } else {
+                                                    return "";
+                                                }
+                                            })}
+                                        </Col>
+                                        <Col key={uuidv4()}>
+                                            <Button
+                                                style={{ width: "100%" }}
+                                                variant="info"
+                                                key={uuidv4()}
+                                                onClick={() => {
+                                                    connect({ connector });
+                                                    handleClose();
+                                                }}
+                                            >
+                                                {connector.name}
+                                                {isPending && ' (connecting)'}
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                ))}
+                            </Stack>
+                        </Container>
+                    </Modal.Body>
+                </Modal>
+            </div>
+            <div key={"topDiv"}>
+                <div key={"navBarKey"} className="navBar">
+                    <Navbar key="navBar" bg="dark" variant="dark" expand="lg" className={styles.navbar}>
+                        <Container fluid>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', height: '100%' }}>
+                                <div style={{ flex: '0 0 200px', display: 'flex', alignItems: 'center' }}>
+                                    <Link to="/" style={{ textDecoration: 'none' }}>
+                                        <Navbar.Brand key="navTitle" style={{ margin: 0, padding: 0 }}>
+                                            <div className={styles.clocktower_brand}>Clocktower</div>
+                                        </Navbar.Brand>
+                                    </Link>
+                                </div>
+                                <div style={{ flex: '1', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', paddingLeft: '20px' }}>
+                                    <Button variant="outline-info" className={styles.account_button} onClick={handleOnClickAccount}>Account</Button>
+                                    <Button variant="outline-info" className={styles.subscriptions_button} onClick={handleOnClickSubscriptions}>Subscriptions</Button>
+                                    <Button variant="outline-info" className={styles.calendar_button} onClick={handleOnClickCalendar}>Calendar</Button>
+                                    {account === adminAccount && <Button variant="outline-info" style={{ margin: "5px" }} onClick={handleOnClickAdmin}>Admin</Button>}
+                                </div>
+                                <div style={{ flex: '0 0 400px', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', gap: '20px', paddingRight: '20px' }}>
+                                    {chains.length > 1 ?
+                                        <NavDropdown title={<span className={styles.chain_pulldown}>Chain: <Icon key={uuidv4()} icon={CHAIN_LOOKUP[selectedChain].icon}></Icon> {CHAIN_LOOKUP[selectedChain].displayName} </span>} id="basic-nav-dropdown" style={{ marginRight: 'auto' }}>
+                                            {chains.map((chain) => (
+                                                <NavDropdown.Item key={uuidv4()} className={styles.chain_pulldown2}>
+                                                    {CHAIN_LOOKUP.map((lchain) => {
+                                                        if (lchain.id === chain.id) {
+                                                            return <Icon key={uuidv4()} className={styles.chain_icon} icon={lchain.icon}></Icon>;
+                                                        } else {
+                                                            return "";
+                                                        }
+                                                    })}
+                                                    <Button variant="outline-info" key={chain.id} onClick={() => changeChain(chain.id)}>
+                                                        {chain.name}
+                                                    </Button>
+                                                </NavDropdown.Item>
+                                            ))}
+                                        </NavDropdown>
+                                        :
+                                        <Navbar.Text style={{ margin: 0, whiteSpace: 'nowrap', marginRight: 'auto' }}>
+                                            <span className={styles.chain_pulldown}>Chain: <Icon key={uuidv4()} icon={CHAIN_LOOKUP[0].icon}></Icon> {CHAIN_LOOKUP[0].displayName} </span>
+                                        </Navbar.Text>
+                                    }
+                                    <ThemeToggle />
+                                    <Nav key="nav" style={{ margin: 0, whiteSpace: 'nowrap' }}>
+                                        {isConnected && !isDisconnected ?
+                                            (<Navbar.Text className={styles.account_text_nav} style={{ margin: 0 }}>Account: {address?.slice(0, 5) + "..." + address?.slice(37, 42)}</Navbar.Text>)
+                                            :
+                                            (<Button variant="outline-success" className={styles.wallet_button} onClick={() => walletButtonClick()}>Sign in Wallet</Button>)
+                                        }
+                                    </Nav>
+                                </div>
+                            </div>
+                        </Container>
+                    </Navbar>
+                </div>
+                <div key={"mainDiv"} id="detail" className="mainDiv">
+                    {!isConnected ? (
+                        <Alert className={`text-center ${styles.connect_wallet_alert}`} variant="info">Please Connect Wallet</Alert>
+                    ) : supportedChainIds.includes(chainId || 0) ? (
+                        <Outlet context={[account]} />
+                    ) : (
+                        <div className="alertDiv">
+                            <Alert variant="danger" className="text-center">Please Connect Back to Supported Chain</Alert>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </ApolloProvider>
+    );
+};
+
+export default Root;

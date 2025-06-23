4b4fcf84a91076b92d076c02540eec6b29c1bec2
Hugo Marx <hugo@clocktower.finance>
2025-06-23 17:13:31 -0500

    removed jwt endpoint

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,6 @@ export const INFINITE_APPROVAL = BigInt(Math.pow(2, 255));
 export const ADMIN_ACCOUNT = "0xeaB2c4C12eac9EC14EA615E721c9d0D9027Fa85a";
 
 export const DOMAIN = "https://app.clocktower.finance";
-export const JWT_SERVER = "https://clocktowerdev.com/api/requesttoken";
 
 interface Token {
     address: `0x${string}`;

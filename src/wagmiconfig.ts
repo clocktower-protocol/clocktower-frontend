243da69d0db26429768d791f62c427aac07ddaaa
vhmarx <vhmarx@haderachllc.com>
2025-08-18 10:48:13 -0500

    added wallet connect and icons

--- src/wagmiconfig.ts
+++ src/wagmiconfig.ts
@@ -1,7 +1,7 @@
 import { createConfig, http, fallback, Config } from 'wagmi'
 import { baseSepolia } from 'wagmi/chains'
 import { base } from 'wagmi/chains'
-import { coinbaseWallet, metaMask } from 'wagmi/connectors'
+import { coinbaseWallet, metaMask, walletConnect } from 'wagmi/connectors'
 
 const transport1: string = import.meta.env.VITE_TRANSPORT1
 const transport2: string = import.meta.env.VITE_TRANSPORT2
@@ -11,6 +11,16 @@ export const config: Config = createConfig({
     connectors: [
       metaMask(),
       coinbaseWallet(),
+      walletConnect({
+        projectId: '0168b93563a3f610bb1c0ff1f29444bb',
+        metadata: {
+          name: 'Clocktower',
+          description: 'Clocktower Protocol',
+          url: 'https://app.clocktower.finance',
+          //icons: ['https://app.clocktower.finance/clocktower-icon.svg'],
+          icons: []
+        }
+      }),
     ],
     transports: { 
       [baseSepolia.id]: fallback([
